dc6c6963ee4b486eb1c9da41aca4c4ecc8d95355
Victor Fournier <victor.fournier.06@orange.fr>
2026-09-12 14:42:57 +0200

    feat: navigation to hub from sub-areas using 'start'

--- scenes/game/area_manager.gd
+++ scenes/game/area_manager.gd
@@ -10,5 +10,14 @@ const AREA_MAPPING := {
 	Area.SHOP : "res://scenes/areas/shop.tscn",
 }
 
-func switch_area(area: Area) -> void:
-	get_tree().change_scene_to_file(AREA_MAPPING[area])
+var origin_area: Area = Area.FISHING
+var current_area: Area = Area.HUB
+
+func switch_area(new_area: Area) -> void:
+	get_tree().change_scene_to_file(AREA_MAPPING[new_area])
+	origin_area = current_area
+	current_area = new_area
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("start") and current_area != Area.HUB:
+		switch_area(Area.HUB)
