0634595d839addc840abee3b42a6fc33d61152a5
Victor Fournier <victor.fournier.06@orange.fr>
2026-09-12 02:20:48 +0200

    feat: basic inventory management

---
+++ scenes/game/inventory.gd
@@ -0,0 +1,43 @@
+extends Node
+
+signal money_changed(new_value: int)
+signal items_changed(new_items: Array[Item])
+
+enum Item { OLD_GOLD }
+
+var money: int:
+	get:
+		return _money
+
+var items: Array[Item]:
+	get:
+		var read_only_items: Array[Item] = _items.duplicate()
+		read_only_items.make_read_only()
+		return read_only_items
+
+var _money: int = 0
+var _items: Array[Item] = []
+
+func spend_money(cost: int) -> bool:
+	assert(cost >= 0)
+	if money >= cost:
+		_money -= cost
+		money_changed.emit(money)
+		return true
+	return false
+
+func earn_money(amount: int) -> void:
+	assert(amount >= 0)
+	_money += amount
+	money_changed.emit(money)
+
+func use_item(item: Item) -> bool:
+	if item in _items:
+		_items.erase(item)
+		items_changed.emit(items)
+		return true
+	return false
+
+func add_item(item: Item) -> void:
+	_items.append(item)
+	items_changed.emit(items)
