7ab9de06abd6ffcbe3d70f2ad23ba5d14310dd50
Victor Fournier <victor.fournier.06@orange.fr>
2026-09-13 16:21:58 +0200

    feat: alpha-based mask for the restoring to scrub only on the object

--- scenes/areas/restorable_object.gd
+++ scenes/areas/restorable_object.gd
@@ -4,9 +4,12 @@ extends Sprite2D
 const GRID_SIZE: int = 16
 const CLEAN_THRESHOLD: float = 0.75
 
-var grid_image: Image
-var cleaning_grid: ImageTexture
+var grid_image: Image #just there to update the ImageTexture
+var cleaning_grid: ImageTexture #the square mask of 0->1s that gets cleaned
+
 var current_item: Item
+var current_item_shape_mask: Image #based on alpha, GRID_SIZE
+var current_item_surface: int
 
 func _ready() -> void:
 	grid_image = Image.create_empty(GRID_SIZE, GRID_SIZE, false, Image.FORMAT_RGBA8)
@@ -23,14 +26,34 @@ func _reset() -> void:
 		return
 	texture = current_item.type.dirty_texture
 	material.set_shader_parameter("clean_texture", current_item.type.clean_texture)
+	_set_current_item_shape()
 
-func scrub_at(world_position: Vector2, scrub_kernel: Array) -> void:
-	if current_item == null:
-		return
+func _set_current_item_shape():
+	current_item_shape_mask = Image.create_empty(GRID_SIZE, GRID_SIZE, false, Image.FORMAT_RGBA8)
+	var current_item_image = texture.get_image()
+	var surface_sum: int = 0 #counts the number of pixels of the mask
 
-	var local_position: Vector2 = to_local(world_position)
+	for x in range(current_item_image.get_width()):
+		for y in range(current_item_image.get_height()):
+			var cell_position = to_cell_position(Vector2(x, y))
+			if current_item_image.get_pixel(x, y).a > 0:
+				current_item_shape_mask.set_pixel(cell_position.x, cell_position.y, Color.WHITE)
+				surface_sum += 1
+	current_item_surface = surface_sum
+
+func to_cell_position(local_position: Vector2) -> Vector2i:
 	var uv_position: Vector2 = local_position / texture.get_size()
 	var cell_position = Vector2i(uv_position * GRID_SIZE)
+	return cell_position
+
+func scrub_at(world_position: Vector2, scrub_kernel: Array) -> bool:
+	var has_something_been_scrubbed: bool = false
+
+	if current_item == null:
+		return has_something_been_scrubbed
+
+	var local_position: Vector2 = to_local(world_position)
+	var cell_position = to_cell_position(local_position)
 
 	var scrub_size: Vector2i = Vector2i(scrub_kernel[0].size(), scrub_kernel.size())
 	var scrub_top_left: Vector2i = cell_position - scrub_size / 2
@@ -46,18 +69,26 @@ func scrub_at(world_position: Vector2, scrub_kernel: Array) -> void:
 			var new_color_value: float = clampf(current_color_value + scrubbed_amount, 0.0, 1.0)
 			var new_color: Color = Color(new_color_value, new_color_value, new_color_value)
 			grid_image.set_pixel(x, y, new_color)
+
+			if current_item_shape_mask.get_pixel(x, y).r > 0.0 and new_color_value > current_color_value:
+				has_something_been_scrubbed = true
+
 	cleaning_grid.update(grid_image)
 
 	if average(grid_image) > CLEAN_THRESHOLD:
 		current_item.deterioration = Item.Deterioration.NONE
 		_reset()
 
-func average(image: Image) -> float:
-	var image_width = image.get_width()
-	var image_height = image.get_height()
+	return has_something_been_scrubbed
 
-	var sum: float = 0.0
-	for x in range(image_width):
-		for y in range(image_height):
-			sum += image.get_pixel(x, y).r
-	return sum / (image_width * image_height)
+#average of the grid image masked by the current item
+func average(grid_sized_image: Image) -> float:
+	assert(grid_sized_image.get_size() == Vector2i(GRID_SIZE, GRID_SIZE))
+
+	var cleanness_sum: float = 0.0
+	for x in range(GRID_SIZE):
+		for y in range(GRID_SIZE):
+			cleanness_sum += grid_sized_image.get_pixel(x, y).r * current_item_shape_mask.get_pixel(x, y).r
+	if current_item_surface == 0:
+		return 0.0
+	return cleanness_sum / current_item_surface
