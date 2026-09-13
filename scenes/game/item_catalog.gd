4bd1f2dad0caeb24b178270e2ce2d89a53a688bc
Victor Fournier <victor.fournier.06@orange.fr>
2026-09-14 01:06:51 +0200

    refacto: replace WIP by TODOs

--- scenes/game/item_catalog.gd
+++ scenes/game/item_catalog.gd
@@ -1,6 +1,7 @@
 class_name ItemCatalog
 
-enum Artefact { OLD_GOLD, AMPHORA, MUSIC_BOX, RUSTY_KEY, MOSSY_CASKET } #WIP
+#TODO: remove once all items have a resource
+enum Artefact { OLD_GOLD, AMPHORA, MUSIC_BOX, RUSTY_KEY, MOSSY_CASKET }
 
 const OLD_GOLD: ItemType = preload("res://resources/items/old_gold.tres")
 const RUSTY_KEY: ItemType = preload("res://resources/items/rusty_key.tres")
