fdad6665536233c6f3081740bf66023fbdea0381
Hlyaus <ck8yph7z48@privaterelay.appleid.com>
2026-09-14 22:29:11 +0200

    Add Dialog Scene !
    Add dialog scene

---
+++ scenes/areas/locating_owners.gd
@@ -0,0 +1,42 @@
+extends Node2D
+
+@export var dialogue: DialogueData
+@export var text_speed := 0.04
+@onready var label: Label = $Label
+
+var current_line := 0
+var typing := false
+
+
+func _ready():
+	show_line()
+
+
+func show_line():
+	if current_line >= dialogue.lines.size():
+		label.text = ""
+		return
+
+	label.text = ""
+	typing = true
+
+	var line = dialogue.lines[current_line]
+
+	for character in line:
+		if not typing:
+			break
+
+		label.text += character
+		await get_tree().create_timer(text_speed).timeout
+
+	typing = false
+
+
+func _input(event):
+	if event.is_action_pressed("ui_accept"):
+		if typing:
+			typing = false
+			label.text = dialogue.lines[current_line]
+		else:
+			current_line += 1
+			show_line()
