7108b97728c5be82b9ac382ca746f122f584a145
Victor Fournier <victor.fournier.06@orange.fr>
2026-09-15 23:01:08 +0200

    feat: sfx during dialog
    # Conflicts:
    #	scenes/areas/locating_owners/locating_owners.gd
    #	scenes/areas/locating_owners/locating_owners.tscn

--- scenes/areas/locating_owners/locating_owners.gd
+++ scenes/areas/locating_owners/locating_owners.gd
@@ -1,14 +1,19 @@
 extends Node2D
 
 @export var dialog: DialogData
-@export var text_speed := 0.04
+@export var text_speed: float = 0.04
+@export var dialog_sfx: AudioStream
+@export var char_per_sfx: int = 3
 @onready var background: Sprite2D = $Background
 @onready var character: Sprite2D = $Character
 
-var current_line_nb := 0
+var current_line_nb: int = 0
 var reveal_tween: Tween
 
+var char_nb_before_last_sfx: int = 0
+
 @onready var label: RichTextLabel = $RichTextLabel
+@onready var sfx_player: SFXPlayer = $SFXPlayer
 
 func _ready() -> void:
 	background.texture=dialog.background
@@ -22,10 +27,15 @@ func show_line() -> void:
 	label.text = dialog.lines[current_line_nb]
 	label.visible_ratio = 0.0
 
-	var count: int = label.get_total_character_count()
-
 	reveal_tween = create_tween()
-	reveal_tween.tween_property(label, "visible_ratio", 1.0, count * text_speed)
+	reveal_tween.tween_property(
+		label,
+		"visible_ratio",
+		1.0,
+		label.get_total_character_count() * text_speed
+	)
+
+	char_nb_before_last_sfx = 0
 
 func _input(event: InputEvent) -> void:
 	if not event.is_action_pressed("ui_accept"):
@@ -37,3 +47,10 @@ func _input(event: InputEvent) -> void:
 	else:
 		current_line_nb += 1
 		show_line()
+
+func _process(_delta: float) -> void:
+	if reveal_tween and reveal_tween.is_running():
+		var char_nb: int = label.visible_characters
+		if char_nb - char_nb_before_last_sfx >= char_per_sfx:
+			char_nb_before_last_sfx = char_nb
+			sfx_player.play_sfx(dialog_sfx, -4.0)
