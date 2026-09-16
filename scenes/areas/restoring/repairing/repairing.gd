269af826a18e20873e092c02b461199714126a18
Victor Fournier <victor.fournier.06@orange.fr>
2026-09-16 22:46:31 +0200

    feat: initial positions and rotations
    but rotations aren't around the center of the piece yet

--- scenes/areas/restoring/repairing/repairing.gd
+++ scenes/areas/restoring/repairing/repairing.gd
@@ -3,13 +3,24 @@ extends Node2D
 
 signal finished_restoring
 
+const ROTATION_NB = 24
+
 var user_interactions_allowed: bool
+var pieces_initial_positions: Array
 
 @onready var pieces: Array = $BrokenItem.get_children()
 
+func _ready() -> void:
+	pieces_initial_positions = pieces.map(func(piece): return piece.position)
+
 func setup(item: Item) -> void:
+	var shuffled_positions: Array = pieces_initial_positions.duplicate()
+	shuffled_positions.shuffle()
+
 	for i in pieces.size():
 		pieces[i].setup(item.type.broken_pieces[i])
+		pieces[i].position = shuffled_positions[i]
+		pieces[i].rotation = (2 * PI / ROTATION_NB) * randi_range(0, ROTATION_NB - 1)
 
 func complete() -> void:
 	pass
