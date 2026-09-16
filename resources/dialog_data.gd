a52bcb352249d0372439b87562afe4df6b860126
Victor Fournier <victor.fournier.06@orange.fr>
2026-09-16 11:15:23 +0200

    feat: moved the character and background out of dialog

--- resources/dialog_data.gd
+++ resources/dialog_data.gd
@@ -2,5 +2,3 @@ class_name DialogData
 extends Resource
 
 @export_multiline var lines: Array[String]
-@export var character: Texture2D
-@export var background: Texture2D
--- scenes/areas/locating_owners/locating_owners.gd
+++ scenes/areas/locating_owners/locating_owners.gd
@@ -1,12 +1,10 @@
 extends Node2D
 
 @export var dialog: DialogData
+@export var background: Sprite2D
+@export var character: Sprite2D
 
-@onready var background: Sprite2D = $Background
-@onready var character: Sprite2D = $Character
 @onready var dialog_component: DialogComponent = $DialogComponent
 
 func _ready() -> void:
-	background.texture = dialog.background
-	character.texture = dialog.character
 	dialog_component.play_dialog(dialog)
