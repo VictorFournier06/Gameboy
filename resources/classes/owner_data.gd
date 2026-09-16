7c74ae4dd37a6d2cff4d15b3ef3603ebe15cfb26
Victor Fournier <victor.fournier.06@orange.fr>
2026-09-16 12:04:54 +0200

    feat: grouped background + character + dialog into a resource

---
+++ resources/classes/owner_data.gd
@@ -0,0 +1,6 @@
+class_name OwnerData
+extends Resource
+
+@export var character: Texture2D
+@export var background: Texture2D
+@export var dialogs: Array[DialogData]
--- scenes/areas/locating_owners/locating_owners.gd
+++ scenes/areas/locating_owners/locating_owners.gd
@@ -1,10 +1,15 @@
 extends Node2D
 
-@export var dialog: DialogData
-@export var background: Sprite2D
-@export var character: Sprite2D
+@export var owners: Array[OwnerData]
 
+@onready var background: Sprite2D = $Background
+@onready var character: Sprite2D = $Character
 @onready var dialog_component: DialogComponent = $DialogComponent
 
 func _ready() -> void:
-	dialog_component.play_dialog(dialog)
+	_display_owner(owners[2], 0)
+
+func _display_owner(input_owner: OwnerData, dialog_nb: int) -> void:
+	background.texture = input_owner.background
+	character.texture = input_owner.character
+	dialog_component.play_dialog(input_owner.dialogs[dialog_nb])
