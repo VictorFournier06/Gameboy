618fe79a5dfbec42d6eed8a3d6505221ffce316e
Victor Fournier <victor.fournier.06@orange.fr>
2026-09-18 22:01:32 +0200

    feat: ability to enter people's houses

---
+++ scenes/areas/locating_owners/door.gd
@@ -0,0 +1,3 @@
+extends Area2D
+
+@export var house_owner: OwnerData #goes to hub when empty
--- scenes/areas/locating_owners/locating_owners.gd
+++ scenes/areas/locating_owners/locating_owners.gd
@@ -6,13 +6,30 @@ extends Node2D
 
 @onready var background: Sprite2D = $Interior/Background
 @onready var character: Sprite2D = $Interior/Character
-@onready var dialog_component: DialogComponent = $DialogComponent
+@onready var dialog_component: DialogComponent = $Interior/DialogComponent
+@onready var overworld: Node2D = $Overworld
+@onready var interior: Node2D = $Interior
+@onready var player: CharacterBody2D = $Overworld/OverworldPlayer
+@onready var camera: Camera2D = $Overworld/OverworldPlayer/Camera2D
 
 func _ready() -> void:
-	_display_owner(owners[2], 0)
+	for door in $Overworld/Doors.get_children():
+		door.body_entered.connect(_enter_door.bind(door))
+
+func _enter_door(element: CharacterBody2D, door: Area2D) -> void:
+	if element != player:
+		return
+	if not door.house_owner:
+		AreaManager.switch_area(AreaManager.Area.HUB)
+	else:
+		player.set_physics_process(false)
+		camera.enabled = false
+		overworld.hide()
+		interior.show()
+		_display_owner(door.house_owner, 0)
 
 func _display_owner(input_owner: OwnerData, dialog_nb: int) -> void:
 	background.texture = input_owner.background
 	character.texture = input_owner.character
-	dialog_component.dialog_finished.connect(HintMenu.play.bind(start_icon, palette))
+	dialog_component.dialog_finished.connect(HintMenu.play.bind(start_icon, palette), CONNECT_ONE_SHOT)
 	dialog_component.play_dialog(input_owner.dialogs[dialog_nb], palette)
