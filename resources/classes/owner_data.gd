0c8e646303e72059d5e5828efd66ca794cb955bc
Victor Fournier <victor.fournier.06@orange.fr>
2026-09-19 22:10:46 +0200

    feat: item-dialog matchings

--- resources/classes/owner_data.gd
+++ resources/classes/owner_data.gd
@@ -3,4 +3,5 @@ extends Resource
 
 @export var character: Texture2D
 @export var background: Texture2D
-@export var dialogs: Array[DialogData]
+@export var base_dialogs: Array[DialogData]
+@export var returning_item_dialogs: Dictionary[ItemType, DialogData]
