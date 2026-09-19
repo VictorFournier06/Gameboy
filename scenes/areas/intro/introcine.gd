9b04b7e830842ab451e0b6f17608c9c864b63c63
Victor Fournier <victor.fournier.06@orange.fr>
2026-09-19 18:49:34 +0200

    feat: allows debug_skipping the intro

--- scenes/areas/intro/introcine.gd
+++ scenes/areas/intro/introcine.gd
@@ -4,14 +4,26 @@ extends AnimatedSprite2D
 @export var music: AudioStream
 
 var reached_start_screen: bool = false
+var skipped: bool = false
 
 func _ready():
+	Debug.skip_minigame.connect(_skip_intro)
+
 	MusicPlayer.music_transition(music)
 	play("default")
 	# Wait X seconds
 	await get_tree().create_timer(animation_1_duration).timeout
+	if skipped:
+		return
 	play("water")
 	await animation_finished
+	reached_start_screen = true
+	if skipped:
+		return
+	play("titleloop")
+
+func _skip_intro() -> void:
+	skipped = true
 	reached_start_screen = true
 	play("titleloop")
 
