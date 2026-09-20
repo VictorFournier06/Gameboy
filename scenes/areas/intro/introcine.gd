07c61b92d72ae4d56f89ce7e3c1f38271c868d80
Victor Fournier <victor.fournier.06@orange.fr>
2026-09-20 13:11:27 +0200

    feat: GBJAM splash art

--- scenes/areas/intro/introcine.gd
+++ scenes/areas/intro/introcine.gd
@@ -2,13 +2,39 @@ extends AnimatedSprite2D
 
 @export var animation_1_duration: float = 6.0
 @export var music: AudioStream
+@export var gbjam_drop_duration: float = 2.0
+@export var gbjam_idle_wait_time: float = 1.0
+@export var gbjam_sfx: AudioStream
 
 var reached_start_screen: bool = false
 var skipped: bool = false
 
+@onready var gbjam_background: Sprite2D = $"../GBJAMBackground"
+@onready var gbjam_foreground: Sprite2D = $"../GBJAMForeground"
+@onready var sfx_player: SFXPlayer = $"../SFXPlayer"
+
 func _ready():
 	Debug.skip_minigame.connect(_skip_intro)
 
+	hide() #hide during gbjam splash art
+	var tween: Tween = create_tween()
+	tween.tween_property(gbjam_foreground, "position:y", 0.0, gbjam_drop_duration)
+	await tween.finished
+	if skipped:
+		return
+	sfx_player.play_sfx(gbjam_sfx)
+	await sfx_player.finished
+	if skipped:
+		return
+	await get_tree().create_timer(gbjam_idle_wait_time).timeout
+	if skipped:
+		return
+
+	gbjam_background.hide()
+	gbjam_foreground.hide()
+	show()
+
+	# start our intro
 	MusicPlayer.music_transition(music)
 	play("default")
 	# Wait X seconds
@@ -25,6 +51,11 @@ func _ready():
 func _skip_intro() -> void:
 	skipped = true
 	reached_start_screen = true
+	gbjam_background.hide()
+	gbjam_foreground.hide()
+	show()
+	if not MusicPlayer.playing:
+		MusicPlayer.music_transition(music)
 	play("titleloop")
 
 func _unhandled_input(event: InputEvent) -> void:
